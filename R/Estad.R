7f9adfc71febdb7ec54647aee3333c3e1086e89a
Daniel Loera <loerasg@gmail.com>
2015-05-15 22:31:02 -0500

    Borrado
    Primer prueba en Windows10

--- R/Estad.R
+++ R/Estad.R
@@ -1,8 +1,4 @@
 Estad <- function(Nivel){
   if (Nivel==1) {shinyAppDir(system.file("Estadist/Distrib", package="Sofi"))}
   else if (Nivel==2) {shinyAppDir(system.file("Estadist/ggMarginal", package="Sofi"))}
-  #switch(Nivel,
-  #       1==shinyAppDir(system.file("Estadist/Distrib", package="Sofi"))
-  #       2==shinyAppDir(system.file("Estadist/ggMarginal", package="Sofi"))
-  #         )
 }
