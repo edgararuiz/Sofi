71818623d7792b0cfcacb1d43c2dc7f1b5cc6abb
Daniel Loera <loerasg@gmail.com>
2015-05-26 22:44:49 -0500

    Añadidos niveles

--- R/Estad.R
+++ R/Estad.R
@@ -1,8 +1,7 @@
 Estad <- function(Nivel){
-  if (Nivel==1) {shinyAppDir(system.file("Estadist/Distrib", package="Sofi"))}
-  else if (Nivel==2) {shinyAppDir(system.file("Estadist/ggMarginal", package="Sofi"))}
-  #switch(Nivel,
-  #       1==shinyAppDir(system.file("Estadist/Distrib", package="Sofi"))
-  #       2==shinyAppDir(system.file("Estadist/ggMarginal", package="Sofi"))
-  #         )
+  if (Nivel==1) {shinyAppDir(system.file("Estadist/Cal_Dis", package="Sofi"))}
+  else if (Nivel==2) {shinyAppDir(system.file("Estadist/Distrib", package="Sofi"))}
+  else if (Nivel==3) {shinyAppDir(system.file("Estadist/Estas", package="Sofi"))}
+  else if (Nivel==4) {shinyAppDir(system.file("Estadist/ggMarginal", package="Sofi"))}
+
 }
