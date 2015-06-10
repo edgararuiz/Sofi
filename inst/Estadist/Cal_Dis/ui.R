d5508f408fed1d24bbd7f60e3bb42f38b6e60f5f
Daniel Loera <loerasg@gmail.com>
2015-06-10 16:17:46 -0500

    Cambios para informacion

--- inst/Estadist/Cal_Dis/ui.R
+++ inst/Estadist/Cal_Dis/ui.R
@@ -42,27 +42,40 @@ shinyUI(pageWithSidebar(
     uiOutput("b_CalDis"),
     
     withMathJax(),  # include the MathJax library
-    p("Recuerda que la función de probabilidad de la distribución binomial es:
+    
+    conditionalPanel(condition = 'input.dist_CalDis === "rnorm"',
+                     p("Para tipificar una variable X que sigue una distribución $$N( \\mu , \\sigma )$$  se usa:
+      $$Z=\\frac { X-\\mu  }{ \\sigma  } $$"),
+                     p("μ es la media"),
+                     p("σ desviación típica."),
+                     tags$hr(),
+                     helpText("Para saber más sobre la distribución normal leer ", 
+                              a(href="http://es.wikipedia.org/wiki/Distribuci%C3%B3n_normal", target="_blank", "esto,"),
+                              " Y recuerda visitar la página de", a(href="http://www.inegi.info/sofi", target="_blank", "Sofi."))
+                    ),
+    conditionalPanel(condition = 'input.dist_CalDis === "rbinom"',
+                     p("Recuerda que la función de probabilidad de la distribución binomial es:
       $$p(X=k)=\\left( \\begin{matrix} n \\\\ k \\end{matrix} \\right) { p }^{ k }\\bullet { q }^{ n-k }$$"),
-    p("n es el número de pruebas."),
-    p("k es el número de éxitos."),
-    p("p es la probabilidad de éxito."),
-    p("q es la probabilidad de fracaso."),
-    p("Y el número combinatorio: $$\\left( \\begin{matrix} n \\\\ k \\end{matrix} \\right) =\\frac { n! }{ k!(n-k)! }$$"),
-    #helpText("Some math here $$Y = \\beta_0 + \\beta_1 x + \\epsilon$$"),
-    #selectInput("x", "Construir un modelo de regresión de mpg en contra:",choices = names(mtcars)[-1]),
-    br(),
+                     p("n es el número de pruebas."),
+                     p("k es el número de éxitos."),
+                     p("p es la probabilidad de éxito."),
+                     p("q es la probabilidad de fracaso."),
+                     p("Y el número combinatorio: $$\\left( \\begin{matrix} n \\\\ k \\end{matrix} \\right) =\\frac { n! }{ k!(n-k)! }$$"),
+                     tags$hr(),
+                     helpText("Para saber más sobre la distribución binomial leer ", 
+                              a(href="http://es.wikipedia.org/wiki/Distribuci%C3%B3n_binomial", target="_blank", "esto,"),
+                              " Y recuerda visitar la página de", a(href="http://www.inegi.info/sofi", target="_blank", "Sofi."))
+                     ),
     tags$hr(),
-    br(),
-    p('Esta aplicación esta en desarrollo gracias a el útil código de ',a(href="https://github.com/mine-cetinkaya-rundel", "Mine Cetinkaya-Rundel"),' asi 
-    como de los paquetes ',a(href="http://cran.rstudio.com/web/packages/shiny/index.html", "shiny"),' y 
+        p('Esta aplicación esta en desarrollo gracias a el útil código de ',a(href="https://github.com/mine-cetinkaya-rundel", "Mine Cetinkaya-Rundel"),' asi 
+                       como de los paquetes ',a(href="http://cran.rstudio.com/web/packages/shiny/index.html", "shiny"),' y 
       ',a(href="http://cran.rstudio.com/web/packages/openintro/index.html", "openintro")
     ),
-    tags$hr(),
-    helpText("Para saber más sobre la distribución binomial leer ", 
-             a(href="http://es.wikipedia.org/wiki/Distribuci%C3%B3n_binomial", target="_blank", "esto,"),
-             " Y recuerda visitar la página de", a(href="http://www.inegi.info/sofi", target="_blank", "Sofi.")),
-    helpText(HTML('<br/>Para dudas y sugerencias escribir a <a href="mailto:jose.loera@inegi.org.mx?subject=Sofi" title="Enviar correo a Daniel">Daniel</a>'))
+    helpText(HTML('<br/>Para dudas y sugerencias escribir a <a href="mailto:jose.loera@inegi.org.mx?subject=Sofi" title="Enviar correo a Daniel">Daniel</a>')
+    )
+    
+    #tags$hr()
+    
     ),
   
   mainPanel(
@@ -88,6 +101,11 @@ shinyUI(pageWithSidebar(
     #actionButton("submit","Enviar"),
     actionButton("newdat","Nuevos datos"),
     actionButton("newEje","Nuevo Ejemplo"),
+    tags$hr(),
+    numericInput("Res_Cuest",
+                 "Y tu respuesta es:",
+                 value = "",
+                 step=0.01),
     h4(textOutput("Error_CalDis")),
     h4(textOutput("area_CalDis"), align = "center"),
     plotOutput("plot_CalDis"),
