9a2a94ebd65c84fbb878d02c0bacd40784148f12
Daniel Loera <loerasg@gmail.com>
2015-06-15 21:50:10 -0500

    Archivos de prueba

---
+++ inst/Estadist/Cal_Dis/Problemas/R_Temperatura.R
@@ -0,0 +1,14 @@
+renderText({
+  if (is.null(Valor()) | is.null(Valor_Cues)) return(NULL)
+  else{
+    #text1 = "¿Crees tener la respuesta correcta? Enviarla "
+    #as.numeric(Valor_Final())
+    #isolate({
+    #errr<-abs(Valor()-input$Res_Cuest)
+    if (length(errr)==0) {return(NULL)}
+    else if (errr<0.005) {text1 = "Felicidades tu respuesta es correcta"}
+    else text1 = text1 = paste("Incorrecto, el valor correcto es: = ",signif(Valor_Cues,3))
+    #})
+    return(text1)
+  }
+})
