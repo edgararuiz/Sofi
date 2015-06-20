6d571ebd38baff4098d11aeaa090360ad4bf8bff
Daniel Loera <loerasg@gmail.com>
2015-06-19 23:58:26 -0500

    Pequeños cambios

--- inst/Estadist/Cal_Dis/ui.R
+++ inst/Estadist/Cal_Dis/ui.R
@@ -18,7 +18,6 @@ shinyUI(pageWithSidebar(
     
 
     br(),
-
     uiOutput("mean_CalDis"),
     uiOutput("sd_CalDis"),
     uiOutput("df1_CalDis"),
@@ -46,6 +45,7 @@ shinyUI(pageWithSidebar(
     conditionalPanel(condition = 'input.dist_CalDis === "rnorm"',
                      p("Para tipificar una variable \\(X\\) que sigue una distribución \\(\\ N(\\mu,\\sigma)\\)  se usa:
       $$Z=\\frac { X-\\mu  }{ \\sigma  } $$"),
+                     p("Donde:"),
                      p("\\(\\mu\\) es la media"),
                      p("\\(\\sigma\\) desviación típica"),
                      tags$hr(),
@@ -56,11 +56,12 @@ shinyUI(pageWithSidebar(
     conditionalPanel(condition = 'input.dist_CalDis === "rbinom"',
                      p("Recuerda que la función de probabilidad de la distribución binomial es:
       $$p(X=k)=\\left( \\begin{matrix} n \\\\ k \\end{matrix} \\right) { p }^{ k }\\bullet { q }^{ n-k }$$"),
+                     p("Donde:"),
                      p("\\(\\ n\\) es el número de pruebas."),
                      p("\\(\\ k\\) es el número de éxitos."),
                      p("\\(\\ p\\) es la probabilidad de éxito."),
                      p("\\(\\ q\\) es la probabilidad de fracaso."),
-                     p("Y el número combinatorio: $$\\left( \\begin{matrix} n \\\\ k \\end{matrix} \\right) =\\frac { n! }{ k!(n-k)! }$$"),
+                     p("Y el coeficiente binomial: $$\\left( \\begin{matrix} n \\\\ k \\end{matrix} \\right) =\\frac { n! }{ k!(n-k)! }$$"),
                      tags$hr(),
                      helpText("Para saber más sobre la distribución binomial leer ", 
                               a(href="http://es.wikipedia.org/wiki/Distribuci%C3%B3n_binomial", target="_blank", "esto,"),
@@ -71,7 +72,7 @@ shinyUI(pageWithSidebar(
                        como de los paquetes ',a(href="http://cran.rstudio.com/web/packages/shiny/index.html", target="_blank", "shiny"),' y 
       ',a(href="http://cran.rstudio.com/web/packages/openintro/index.html", target="_blank", "openintro")
     ),
-    helpText(HTML('<br/>Para dudas y sugerencias escribir a <a href="mailto:jose.loera@inegi.org.mx?subject=Sofi" title="Enviar correo a Daniel">Daniel</a>')
+    helpText(HTML('<br/>Para dudas y sugerencias escribir a él <a href="mailto:jose.loera@inegi.org.mx?subject=Sofi" title="Enviar correo a Daniel">desarrollador</a>')
     )
     
     #tags$hr()
@@ -119,20 +120,7 @@ shinyUI(pageWithSidebar(
     h5(textOutput("status2"),style="font-weight=500; color: #20f;"),
     h5(textOutput("status3"),style="font-weight=500; color: #f00000;"),
     tags$hr(),
-    #conditionalPanel(condition = 'input.Ejem_Dis === "Peso_Est"',
-    #                 #h3("Peso de estudiantes",align = "center"),
-    #                 h4(textOutput("R_Doc_Peso"))
-    #),
-    #conditionalPanel(condition = 'input.Ejem_Dis === "Tiro_Arc"',
-    #                 #h3("Tiro de arco",align = "center"),
-    #                 h4(textOutput("R_Doc_Tiro"))
-    #),
-    #conditionalPanel(condition = 'input.Ejem_Dis === "Temp_Est"',
-    #                 #h3("Temperatura",align = "center"),
-    #                 h4(textOutput("R_Doc_Temp"))
-    #),
     
-    #br(),
     
     #h4(textOutput("Error_CalDis")),
     h4(textOutput("area_CalDis"), align = "center"),
