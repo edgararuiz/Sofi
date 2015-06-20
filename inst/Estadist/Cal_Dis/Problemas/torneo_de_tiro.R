01c371c453d36f37af42f3eb0301f835ac6d7840
Daniel Loera <loerasg@gmail.com>
2015-06-20 09:41:06 -0500

    Globales para tiro

--- inst/Estadist/Cal_Dis/Problemas/torneo_de_tiro.R
+++ inst/Estadist/Cal_Dis/Problemas/torneo_de_tiro.R
@@ -1,15 +1,15 @@
 renderText({
   Distribu<-"rbinom"
-  tiros<- round(runif(1, 5, 45),digits=0)
-  probabi<-round(runif(1, .4, .99),digits=2)
+  tiros<<- round(runif(1, 5, 45),digits=0)
+  probabi<<-round(runif(1, .4, .99),digits=2)
   lower_bound<-sample(c("open", "closed"), 1)
   upper_bound<-sample(c("open", "closed"), 1)
   
   #Cues_Val<-round(rnorm(1, mean=Peso, sd=DS_Peso),digits=1)
-  Bus_Area<-sample(c("lower", "upper", "both", "middle", "equal"), 1)
+  Bus_Area<<-sample(c("lower", "upper", "both", "middle", "equal"), 1)
 
   if (Bus_Area=="lower") {
-    Aciertos<-rbinom(1, tiros, probabi)
+    Aciertos<<-rbinom(1, tiros, probabi)
     if (lower_bound=="closed"){
       Cuestion<-paste("exactamente en ",Aciertos," ocasiones o menos? ")
     }
@@ -20,7 +20,7 @@ renderText({
                                       p_CalDis=probabi, lower_bound_CalDis=lower_bound, upper_bound_CalDis=upper_bound))
   }
   else if(Bus_Area=="upper") {
-    Aciertos<-rbinom(1, tiros, probabi)
+    Aciertos<<-rbinom(1, tiros, probabi)
     if (lower_bound=="closed"){
       Cuestion<-paste("exactamente en ",Aciertos," ocasiones o mas? ")
     }
@@ -32,9 +32,9 @@ renderText({
   }
   else if(Bus_Area=="both") {
     
-    Cues_Val<-rbinom(3, tiros, probabi)
-    a_val<-min(Cues_Val)
-    b_val<-max(Cues_Val)
+    Cues_Val<<-rbinom(3, tiros, probabi)
+    a_val<<-min(Cues_Val)
+    b_val<<-max(Cues_Val)
     if (lower_bound=="closed" & upper_bound=="closed"){
       Cuestion<-paste("exactamente en ",a_val," o menos ocasiones a la vez que pudiera acertar ",b_val," ocasiones exactamente o mas?")
     }
@@ -49,12 +49,11 @@ renderText({
     
     Valor_Cues<<-as.numeric(Valor_Fin(dist_CalDis=Distribu, tail_CalDis=Bus_Area, a_CalDis=a_val, b_CalDis=b_val, n_CalDis=tiros, 
                                       p_CalDis=probabi, lower_bound_CalDis=lower_bound, upper_bound_CalDis=upper_bound))
-    
   }
   else if(Bus_Area=="middle") {
-    Cues_Val<-rbinom(3, tiros, probabi)
-    a_val<-min(Cues_Val)
-    b_val<-max(Cues_Val)
+    Cues_Val<<-rbinom(3, tiros, probabi)
+    a_val<<-min(Cues_Val)
+    b_val<<-max(Cues_Val)
     if (lower_bound=="closed" & upper_bound=="closed"){
       Cuestion<-paste("entre exactamente ",a_val,"  y ",b_val," ?")
     }
@@ -75,7 +74,7 @@ renderText({
     #Cues_Val<-round(rnorm(3, mean=Peso, sd=DS_Peso),digits=0)
     #a_val<-min(Cues_Val)
     #b_val<-max(Cues_Val)
-    Aciertos<-rbinom(1, tiros, probabi)
+    Aciertos<<-rbinom(1, tiros, probabi)
     Cuestion<-paste("exactamente en ",Aciertos," ocasiones?")
     Valor_Cues<<-as.numeric(Valor_Fin(dist_CalDis=Distribu, tail_CalDis=Bus_Area, a_CalDis=Aciertos, n_CalDis=tiros, 
                                       p_CalDis=probabi, lower_bound_CalDis=lower_bound, upper_bound_CalDis=upper_bound))
