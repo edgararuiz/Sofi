858ef8431f73d43524e00965cf2353f25babaeec
Daniel Loera <loerasg@gmail.com>
2015-06-20 09:39:25 -0500

    Update ayuda

--- inst/Estadist/Cal_Dis/Problemas/R_Peso_Estudiantes.R
+++ inst/Estadist/Cal_Dis/Problemas/R_Peso_Estudiantes.R
@@ -5,15 +5,18 @@ renderUI({
     withMathJax(
       helpText('Es simple, lo primero es solo sustituir los valores de la siguiente forma: 
                $$Z=\\frac { X-\\mu  }{ \\sigma  }=\\frac { ',Cues_Val,'-\\',Peso,'  }{ \\',DS_Peso,' }=
-               ',signif((Cues_Val-Peso)/DS_Peso,4),' $$')
+               ',signif((Cues_Val-Peso)/DS_Peso,4),' $$'),
+      helpText('Ahora los pesos siguen una distribución normal con \\(\\mu\\) =0 y \\(\\sigma\\) =1')
       )
   }
   else {
     withMathJax(
       helpText('Es simple, lo primero es solo sustituir los valores de la siguiente forma \\(\\ { Z }_{ 1 }\\): 
                $${ Z }_{ 1 }=\\frac { X-\\mu  }{ \\sigma  }=\\frac { ',a_val,'-\\',Peso,'  }{ \\',DS_Peso,' }=
-               ',signif((a_val-Peso)/DS_Peso,4),' $$ Y para \\(\\ { Z }_{ 2 }\\): $${ Z }_{ 2 }=\\frac { X-\\mu  }{ \\sigma  }=\\frac { ',b_val,'-\\',Peso,'  }{ \\',DS_Peso,' }=
-               ',signif((b_val-Peso)/DS_Peso,4),' $$')
+               ',signif((a_val-Peso)/DS_Peso,4),' $$ Y para \\(\\ { Z }_{ 2 }\\): $${ Z }_{ 2 }=\\frac { X-\\mu  }
+{ \\sigma  }=\\frac { ',b_val,'-\\',Peso,'  }{ \\',DS_Peso,' }=
+               ',signif((b_val-Peso)/DS_Peso,4),' $$'),
+      helpText('Ahora \\(\\ { Z }_{ 1 }\\) y \\(\\ { Z }_{ 2 }\\) siguen una distribución normal con \\(\\mu\\) =0 y \\(\\sigma\\) =1')
       )
   }
   
