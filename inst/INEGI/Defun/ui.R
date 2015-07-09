027be383597c1b3621163f938a62d0078f61c393
Daniel Loera <loerasg@gmail.com>
2015-07-08 20:22:21 -0500

    source

--- inst/INEGI/Defun/ui.R
+++ inst/INEGI/Defun/ui.R
@@ -6,21 +6,30 @@ shinyUI(navbarPage("Defunciones",#theme = shinytheme("readable"),
 #___________________________Etapa 1______________________________
 ####________________________________________________________________                   
 tabPanel("Etapa 1",icon = icon("random"),
-    source(system.file("INEGI/Defun/General/IU_Etapa1.R", package="Sofi"),local=T)$value
+    #source(system.file("INEGI/Defun/General/IU_Etapa1.R", package="Sofi"),local=T,encoding="UTF-8")$value
+    #source(system.file("INEGI/Defun/General/IU_Etapa1.R", package="Sofi"),local=T)$value
+    source('./General/IU_Etapa1.R',local=T,encoding="UTF-8")$value
 ),
 ####_____________________________________________________________
-#___________________________Etapa 2 y 3______________________________
+#___________________________Etapa 2______________________________
 ####________________________________________________________________
 tabPanel("Etapa 2 y 3",
-    source(system.file("INEGI/Defun/General/IU_Etapa2.R", package="Sofi"),local=T)$value
+    source(system.file("INEGI/Defun/General/IU_Etapa2.R", package="Sofi"),local=T,encoding="UTF-8")$value
 ),#tabPanel("Etapa 2 y 3",
+####_____________________________________________________________
+#___________________________Etapa 3______________________________
+####________________________________________________________________
+#tabPanel("Etapa 3",
+#         source(system.file("INEGI/Defun/General/IU_Etapa2.R", package="Sofi"),local=T)$value
+#),#tabPanel("Etapa 2 y 3",
+
 
 ####_____________________________________________________________
 #___________________________Etapa 4 y 5______________________________
 ####________________________________________________________________
 tabPanel("Etapa 4 y 5",
          #fluidPage(
-    source(system.file("INEGI/Defun/General/IU_Etapa4.R", package="Sofi"),local=T)$value
+    source(system.file("INEGI/Defun/General/IU_Etapa4.R", package="Sofi"),local=T,encoding="UTF-8")$value
 )#tabPanel("Etapa 4 y 5",
 
 ))
